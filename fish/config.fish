98059974df0c2fafccad75099ca0d356cb1e9991
muki <muki@kiboigo.com>
2023-10-23 20:50:41 -0700

    add lombok support to jdtls w/ nvim on fish

--- fish/config.fish
+++ fish/config.fish
@@ -2,9 +2,11 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-
 # Zig
 fish_add_path /opt/zig/
+
+# For jdtls (with Lombok)
+set -gx JDTLS_JVM_ARGS "-javaagent:$HOME/.local/share/eclipse/lombok.jar"
 
 # OCaml
 source ~/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true
