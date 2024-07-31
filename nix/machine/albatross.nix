784d2b7cbc05cea1d6358aab38dc5b7c0c08c87f
Muki Kiboigo <muki@kiboigo.com>
2024-07-30 21:10:41 -0700

    add albatross

--- nix/machine/albatross.nix
+++ nix/machine/albatross.nix
@@ -1,8 +1,27 @@
-{ config, pkgs, self, ... }:
+{ config, pkgs, ... }:
 
 {
-  networking.hostName = "albatross";
-  i18n.defaultLocale = "en_US.UTF-8";
+  imports = [
+    ./common.nix
+    ./hardware/albatross.nix
+    ../display/i3.nix
+  ];
+
+  # https://nixos.wiki/wiki/Nvidia
+  hardware.opengl = { enable = true; };
 
   services.xserver.videoDrivers = [ "nvidia" ];
+
+  hardware.nvidia = {
+    modesetting.enable = true;
+    powerManagement.enable = false;
+    powerManagement.finegrained = false;
+    open = false;
+    nvidiaSettings = true;
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+  };
+
+  boot.loader.systemd-boot.enable = true;
+
+  networking.hostName = "albatross";
 }
