6271601ccb7e4d268352a54e51e8f24f36ac6502
Muki Kiboigo <muki@kiboigo.com>
2024-08-13 00:12:08 -0700

    add virtual machine support

--- nix/machine/common.nix
+++ nix/machine/common.nix
@@ -17,11 +17,14 @@
     nssmdns4 = true;
     openFirewall = true;
     publish = {
-        enable = true;
-        userServices = true;
-        addresses = true;
+      enable = true;
+      userServices = true;
+      addresses = true;
     };
   };
+
+  virtualisation.libvirtd.enable = true;
+  programs.virt-manager.enable = true;
 
   programs.zsh.enable = true;
 
@@ -30,7 +33,7 @@
     shell = pkgs.zsh;
     home = "/home/muki";
     initialPassword = "muki";
-    extraGroups = [ "wheel" "networkmanager" "video" ];
+    extraGroups = [ "wheel" "networkmanager" "video" "libvirtd" ];
   };
 
   environment.systemPackages = with pkgs; [ wget curl zip unzip openssl ];
