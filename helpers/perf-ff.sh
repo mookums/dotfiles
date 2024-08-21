e2872ab9dff8a28d030a84b9101e5651a8b0afa9
Muki Kiboigo <muki@kiboigo.com>
2024-08-21 13:51:00 -0700

    add cache to perf helper

--- helpers/perf-ff.sh
+++ helpers/perf-ff.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/env bash
 
-perf record -g -F $1 --call-graph dwarf "${@:2}"
+case $1 in
+    "perf" )
+        perf record -g -F $2 --call-graph dwarf "${@:3}"
+        ;;
+    "cache" )
+        perf stat -B -e cache-references,cache-misses,cycles,instructions,branches,faults,migrations "${@:2}"
+        ;;
+    *)
+        echo "perf or cache"
+        ;;
+esac
