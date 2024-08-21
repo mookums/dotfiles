ef8f881c95c0f7f3afc552e658aca6f76d03d700
Muki Kiboigo <muki@kiboigo.com>
2024-08-20 21:40:26 -0700

    add vega

---
+++ nix/machine/vega.nix
@@ -0,0 +1,27 @@
+{ config, pkgs, ... }:
+
+{
+  imports = [
+    ./common.nix
+    ./hardware/vega.nix
+    ../display/i3.nix
+  ];
+
+  # https://nixos.wiki/wiki/Nvidia
+  hardware.opengl = { enable = true; };
+
+  services.xserver.videoDrivers = [ "nvidia" ];
+
+  hardware.nvidia = {
+    modesetting.enable = true;
+    powerManagement.enable = false;
+    powerManagement.finegrained = false;
+    open = false;
+    nvidiaSettings = true;
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+  };
+
+  boot.loader.systemd-boot.enable = true;
+
+  networking.hostName = "vega";
+}
