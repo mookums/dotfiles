a931b5e6866e718e939f731cc2641318905d41c7
Muki Kiboigo <muki@kiboigo.com>
2024-10-20 04:10:36 -0700

    use curses for gnupg

--- nix/machine/common.nix
+++ nix/machine/common.nix
@@ -13,7 +13,10 @@
   nix.settings.trusted-users = [ "@wheel" ];
 
   services.openssh.enable = true;
-  programs.gnupg.agent.enable = true;
+  programs.gnupg.agent = {
+      enable = true;
+      pinentryPackage = pkgs.pinentry-curses;
+  };
 
   networking.networkmanager.enable = true;
   networking.firewall.enable = true;
