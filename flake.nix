351e2d2956eb441c12b27bfdfbf7d4869f2c1699
Muki Kiboigo <muki@muki.gg>
2026-06-05 08:47:17 -0700

    update to 26.05

--- flake.nix
+++ flake.nix
@@ -2,12 +2,12 @@
   description = "Muki's NixOS :3";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
 
     agenix.url = "github:ryantm/agenix";
     agenix.inputs.nixpkgs.follows = "nixpkgs";
 
-    home-manager.url = "github:nix-community/home-manager/release-25.11";
+    home-manager.url = "github:nix-community/home-manager/release-26.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
